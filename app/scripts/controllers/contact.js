dc5b65105b96f245e569339a63b2dd9f49f9b00f
fairyly <498745097@qq.com>
2016-10-29 10:25:02 +0800

    myangular demo

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularYeomanApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularYeomanApp
+ */
+angular.module('angularYeomanApp')
+  .controller('ContactCtrl', function ($scope) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
